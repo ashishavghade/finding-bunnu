5c96f543d5cca66326e04f6da16f1cc9433f5865
ashishavghade <101986205+ashishavghade@users.noreply.github.com>
2026-02-06 15:38:54 +0530

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const cursor = document.getElementById("heart-cursor");
 
-  let mouseX = 0;
-  let mouseY = 0;
-  let cursorX = 0;
-  let cursorY = 0;
+  let mouseX = window.innerWidth / 2;
+  let mouseY = window.innerHeight / 2;
+  let cursorX = mouseX;
+  let cursorY = mouseY;
 
   const speed = 0.15;
 
